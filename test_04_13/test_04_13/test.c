43ab37d58f795f060b456515ac7d96aeb3ccbadc
蒸鹅心啊 <105346284+SCBruno@users.noreply.github.com>
2022-05-16 23:28:05 +0800

    Add files via upload

---
+++ test_04_13/test_04_13/test.c
@@ -0,0 +1,172 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	/*int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if(18<age<28)
+//	{
+//		printf("成年\n");
+//		printf("可以饮酒\n");
+//	}
+//	else
+//	{
+//		printf("壮年\n");
+//	}*/
+//
+//
+//
+//
+//
+//
+//	/*int age = 15;
+//	if (age < 15)
+//		printf("未成年\n");
+//	else if (age > 18 && age < 28)
+//	{
+//		printf("成年\n");
+//		printf("可以饮酒\n");
+//	}
+//	else
+//		printf("壮年\n");*/
+//
+//
+//	//int a = 2;
+//	//int b = 3;
+//	//if (a == 3)
+//	//	if (b == 1)
+//	//		printf("hehe\n");
+//	//else
+//	//	printf("haha\n");
+//
+//
+//	/*int a, b;
+//	char c;
+//	float money;
+//	scanf("%d %d %c", &a, &b, &c);
+//	if (c == 'e')
+//	{
+//		switch(b)
+//		{
+//			case 90:
+//				money = 6.95 * a * 0.97;
+//				break;
+//			case 93:
+//				money = 7.44 * a * 0.97;
+//				break;
+//			case 97:
+//				money = 7.93 * a * 0.97;
+//				break;
+//		}
+//	}
+//	if (c == 'm')
+//	{
+//		switch (b)
+//		{
+//		case 90:
+//			money = 6.95 * a * 0.95;
+//			break;
+//		case 93:
+//			money = 7.44 * a * 0.95;
+//			break;
+//		case 97:
+//			money = 7.93 * a * 0.95;
+//			break;
+//		}
+//	}
+//	printf("%.2f", money);*/
+//
+//	//int i = 0;
+//	//scanf("%d ", &i);
+//	//printf("%d", i);
+//
+//	/*int  a, b;
+//	scanf("%d", &a);
+//	scanf("%d", &b);
+//	printf("%d,%d", a, b);*/
+//
+//
+//
+//	int a = 2;
+//	int b = 3;
+//
+//	
+//
+//	return 0;
+//}
+
+
+
+//
+//int main()
+//{
+//    int n = 1;
+//    int m = 2;
+//    switch (n)
+//    {
+//    case 1:m++;
+//    case 2:n++;
+//    case 3:
+//        switch (n)
+//        {
+//        case 1: n++;
+//        case 2: m++; n++;
+//            break;
+//        }
+//    case 4:
+//        m++;
+//        break;
+//    default:
+//        break;
+//    }
+//    printf("m = %d, n = %d\n", m, n);
+//    return 0;
+//}
+
+int main()
+{
+	//输入1 :输出星期一
+	//输入2 :输出星期二
+	//输入3 :输出星期三
+	//输入4 :输出星期四
+	//输入5 :输出星期五
+	//输入6 :输出星期六
+	//输入7 :输出星期天
+	/*int day = 0;
+	scanf("%d", &day);
+	switch (day)
+	{
+	case 1:
+		printf("星期一");
+		break;
+	case 2:
+		printf("星期二");
+		break;
+	case 3:
+		printf("星期三");
+		break;
+	case 4:
+		printf("星期四");
+		break;
+	case 5:
+		printf("星期五");
+		break;
+	case 6:
+		printf("星期六");
+		break;
+	case 7:
+		printf("星期天");
+		break;
+
+	
+	}*/
+
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+
+	return 0;
+}
